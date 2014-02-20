719f0e8cd6a7996b527a8dd96b53c849922ba078
Francis Chong <francis@ignition.hk>
2014-02-21 00:01:48 +0800

    Initial Commit

---
+++ MasonryGenerator/IGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  IGAppDelegate.h
+//  MasonryGenerator
+//
+//  Created by Francis Chong on 2/21/14.
+//  Copyright (c) 2014 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MasonryGenerator/IGViewController.h
@@ -0,0 +1,13 @@
+//
+//  IGViewController.h
+//  MasonryGenerator
+//
+//  Created by Francis Chong on 2/21/14.
+//  Copyright (c) 2014 Ignition Soft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGViewController : UIViewController
+
+@end
